d9550b759daffecdab2b8ee2659043dc71df5eb7
Dulio Denis <dulio.denis@yahoo.com>
2015-11-06 13:24:25 -0500

    Added animated monster

--- PocketMonster/PocketMonster/ViewController.swift
+++ PocketMonster/PocketMonster/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var imageArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imageArray.append(img!)
+        }
+        
+        monsterImage.animationImages = imageArray
+        monsterImage.animationDuration = 0.8
+        monsterImage.animationRepeatCount = 0
+        monsterImage.startAnimating()
+    }
 
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pocket Monster
-An iOS 9 [tamagotchi](https://en.wikipedia.org/wiki/Tamagotchi) digital pet monster written in Swift 2. 
+An iOS 9 [tamagotchi](https://en.wikipedia.org/wiki/Tamagotchi) virtual pet monster written in Swift 2. 
+
+![](https://github.com/duliodenis/pocketmonster/blob/master/art/pocketmonster.gif)
 
 ### Licensing
 Pocket Monster is licensed under [the MIT License](https://github.com/duliodenis/pocketmonster/blob/master/LICENSE).
